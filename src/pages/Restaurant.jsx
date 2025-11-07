0719b361378d9f11522ec38c3bedcd5aa48f2212
Ugyen Dorji <udeeandres2020@gmail.com>
2025-11-07 21:51:38 +0530

    done with restaurant page

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -1,10 +1,38 @@
 import Search from "../components/Search";
-import PopularResto from "../components/PopularResto";
+import RestoCarousal from "../components/RestoCarousal";
+import useFetchResto from "../hooks/useFetchResto";
+import CarousalCard from "../components/CarousalCard";
 const Restaurant = () => {
+  const restObject = useFetchResto();
+  const restaurants =
+    restObject?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+      ?.restaurants || [];
+  const restList = restaurants;
   return (
     <section>
       <Search />
-      <PopularResto />
+      <div>
+        <div className="px-80 pb-5">
+          <hr className=" border-blue-500/10 pb-7 " />
+          <h3 className="text-lg font-semibold ">Popular Restaurants</h3>
+        </div>
+        <div className="scale-93">
+          <RestoCarousal />
+        </div>
+      </div>
+      <div className="px-80 pb-5 pt-5">
+        <hr className=" border-blue-500/10 pb-7 " />
+        <h3 className="text-lg font-semibold ">All Restaurants</h3>
+      </div>
+      <div className="ml-70 mr-60 pt-10">
+        <div className="flex gap-4 flex-wrap scale-93">
+          {restList.map((res) => (
+            <div key={res.info.id} className="mb-18">
+              <CarousalCard restData={res} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
